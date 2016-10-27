d176e0813ec4e36fd93e4bdfb236f697ea3136f9
Roman <yakovlevroman8@gmail.com>
2016-10-27 23:02:29 +0700

    Update github_trending.py

--- github_trending.py
+++ github_trending.py
@@ -14,15 +14,15 @@ def get_trending_repositories(top_size):
 def get_open_issues_amount(repo_owner, repo_name):
     git_request = requests.get('https://api.github.com/repos/{}/{}/issues'.format(repo_owner,repo_name))
     repo_issues_list = git_request.json()
-    return [x['url'] for repo_issue in repo_issues_list if 'pull_request' not in repo_issue]
+    return [repo_issue['url'] for repo_issue in repo_issues_list if 'pull_request' not in repo_issue]
 
 
 if __name__ == '__main__':
     top_size = 20
     repos_list, week_ago = get_trending_repositories(top_size)
     print('Most popular github repositories created since {} and after\n'.format(week_ago))
-    for x in repos_list:
-        repo_owner, repo_name, stars_amount = x['owner']['login'], x['name'], x['stargazers_count']
+    for repo_obj in repos_list:
+        repo_owner, repo_name, stars_amount = repo_obj['owner']['login'], repo_obj['name'], repo_obj['stargazers_count']
         issues_list = get_open_issues_amount(repo_owner, repo_name)
         print('Repo name: {}, stars: {}\nIssues:'.format(repo_name, stars_amount))
         if not issues_list:
