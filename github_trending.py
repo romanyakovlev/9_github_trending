045b0936e37134de473bd9113ede567c0ff431ae
Roman Yakovlev <yakovlevroman8@gmail.com>
2016-11-25 02:08:42 +0700

    improve code readability

--- github_trending.py
+++ github_trending.py
@@ -4,9 +4,11 @@ import datetime
 
 def get_trending_repositories(top_size):
     today = datetime.date.today()
-    one_week = datetime.timedelta(days=7)
+    days_amount = 7
+    one_week = datetime.timedelta(days = days_amount)
     week_ago = str(today - one_week)
-    git_request = requests.get('https://api.github.com/search/repositories?q=created:>={}&sort=string'.format(week_ago))
+    get_params = {'q' : 'created:>={}'.format(week_ago), 'sort' : 'string'}
+    git_request = requests.get('https://api.github.com/search/repositories', params = get_params)
     repos_list = git_request.json()['items'][:top_size]
     return repos_list, week_ago
 
@@ -17,10 +19,7 @@ def get_open_issues_amount(repo_owner, repo_name):
     return [repo_issue['url'] for repo_issue in repo_issues_list if 'pull_request' not in repo_issue]
 
 
-if __name__ == '__main__':
-    top_size = 20
-    repos_list, week_ago = get_trending_repositories(top_size)
-    print('Most popular github repositories created since {} and after\n'.format(week_ago))
+def output_repos_issues(repos_list):
     for repo_obj in repos_list:
         repo_owner, repo_name, stars_amount = repo_obj['owner']['login'], repo_obj['name'], repo_obj['stargazers_count']
         issues_list = get_open_issues_amount(repo_owner, repo_name)
@@ -29,3 +28,10 @@ if __name__ == '__main__':
             print('No issues')
         for issue in issues_list:
             print(issue)
+
+
+if __name__ == '__main__':
+    top_size = 20
+    repos_list, week_ago = get_trending_repositories(top_size)
+    print('Most popular github repositories created since {} and after\n'.format(week_ago))
+    output_repos_issues(repos_list)
